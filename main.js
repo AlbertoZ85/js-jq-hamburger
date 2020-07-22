f9187c427757ddfa164d5d68e7a1432e7f7c061c
Alberto Zordan <zorberto@gmail.com>
2020-07-22 18:29:18 +0200

    I see what you did there...
    Metodo add/remove class, c'era la classe "active" in fondo al CSS

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+// Con HIDE e SHOW
+// $('.header-right i').click(function(){
+//     $('.hamburger-menu').show('slow');
+// });
+//
+// $('.close i').click(function(){
+//     $('.hamburger-menu').hide('slow');
+// });
+
+// Con ADD/REMOVE CLASS
 $('.header-right i').click(function(){
-    $('.hamburger-menu').show('slow');
+    $('.hamburger-menu').addClass('active');
 });
 
 $('.close i').click(function(){
-    $('.hamburger-menu').hide('slow');
+    $('.hamburger-menu').removeClass('active');
 });
