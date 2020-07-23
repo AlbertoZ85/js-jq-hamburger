016c30bb393fca59c7ac31b0eb8aa2c3f9e26154
Alberto Zordan <zorberto@gmail.com>
2020-07-23 10:01:53 +0200

    Some tweaks and comments...

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 // Con HIDE e SHOW
-// $('.header-right i').click(function(){
+// $('.header-right > a').click(function(){
 //     $('.hamburger-menu').show('slow');
 // });
 //
-// $('.close i').click(function(){
+// $('.close').click(function(){
 //     $('.hamburger-menu').hide('slow');
 // });
 
+// L'elemento 'a' ha un line-height, prende tutta l'altezza
+// del header, quindi l'area cliccabile è più estesa. Se voglio
+// concentrare l'azione solo sull'icona aggiungo 'i' nel selettore
+
 // Con FADE IN/OUT
-// $('.header-right i').click(function(){
+// $('.header-right > a').click(function(){
 //     $('.hamburger-menu').fadeIn('slow');
 // });
 //
-// $('.close i').click(function(){
+// $('.close').click(function(){
 //     $('.hamburger-menu').fadeOut('slow');
 // });
 
@@ -20,10 +24,10 @@
 // quando hanno entrambi specificato il tempo della transizione. Pare che, per fare un esempio, entrambi fadeIn e show cambino il display da 'none' a 'block' e opacity parta da 0 e gradualmente vada verso 1; la differenza sta in width e height: con fade NON cambiano, mentre con show partono da 0 e gradualmente vanno verso 'auto'
 
 // Con ADD/REMOVE CLASS
-$('.header-right i').click(function(){
+$('.header-right > a').click(function(){
     $('.hamburger-menu').addClass('active');
 });
 
-$('.close i').click(function(){
+$('.close').click(function(){
     $('.hamburger-menu').removeClass('active');
 });
